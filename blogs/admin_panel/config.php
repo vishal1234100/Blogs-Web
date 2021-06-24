8fa32e2544d82a3551b5abfb289a4f0531254f80
Vishal mote <55905025+vishal1234100@users.noreply.github.com>
2021-06-24 14:17:01 +0530

    Update config.php

--- blogs/admin_panel/config.php
+++ blogs/admin_panel/config.php
@@ -3,7 +3,7 @@
 	$name = "root";
 	$pass = "";
 	$host = "localhost";
-	$dbname = "blogs";
+	$dbname = "blog";
 
 	$conn = mysqli_connect($host,$name,$pass,$dbname) or die("connection failed :" .mysqli_connect_error());
 
